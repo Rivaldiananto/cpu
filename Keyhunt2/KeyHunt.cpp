75dd626e6dcdc7209b79954fe2b174a4f55ee50b
Rivaldiananto <rivaldiananto@gmail.com>
2024-06-20 01:09:53 +0700

    Update KeyHunt.cpp

--- Keyhunt2/KeyHunt.cpp
+++ Keyhunt2/KeyHunt.cpp
@@ -20,23 +20,6 @@ using namespace std;
 Point Gn[CPU_GRP_SIZE / 2];
 Point _2Gn;
 
-// Function to convert binary string to hexadecimal string
-string binToHex(const string& bin) {
-    static const char* const lut = "0123456789ABCDEF";
-    size_t len = bin.length();
-
-    if (len % 4 != 0) throw invalid_argument("Binary string length must be a multiple of 4");
-
-    string hex;
-    hex.reserve(len / 4);
-
-    for (size_t i = 0; i < len; i += 4) {
-        int nibble = (bin[i] - '0') << 3 | (bin[i + 1] - '0') << 2 | (bin[i + 2] - '0') << 1 | (bin[i + 3] - '0');
-        hex.push_back(lut[nibble]);
-    }
-
-    return hex;
-}
 
 // ----------------------------------------------------------------------------
 
